b92f2c20d3aa6dc6ddf6f582b24660f3ddbea10c
siverchen <siverchen@gmail.com>
2013-03-08 11:29:39 +0800

    initialize command

---
+++ OMCommand/OMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OMAppDelegate.h
+//  OMCommand
+//
+//  Created by Chen Lei on 13-3-8.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OMCommand/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OMCommand
+//
+//  Created by Chen Lei on 13-3-8.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OMAppDelegate class]));
+    }
+}
